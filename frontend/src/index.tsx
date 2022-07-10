f8d12d8ecbcdcc745d51087e33d6b9c2cf58f59b
AndriyCh1 <chekanovskiyacc@gmail.com>
2022-07-10 15:01:54 +0300

    feat: connect scss, add color variables

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './index.css';
 import { render } from 'react-dom';
+import './assets/scss/styles.scss';
 
 render(
   <React.StrictMode>
+    <div></div>
   </React.StrictMode>,
   document.getElementById('root')
 );
