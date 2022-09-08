909540a92c1a8766912ab18397bbc909966e7ce1
AndriyCh1 <chekanovskiyacc@gmail.com>
2022-09-08 19:15:10 +0300

    feat: search quizzes functionality on visitor page

--- frontend/src/pages/visitor-home.tsx
+++ frontend/src/pages/visitor-home.tsx
@@ -7,22 +7,41 @@ import QuizList from '../components/quiz-list';
 import { BsSearch as SearchIcon } from 'react-icons/bs';
 
 import quizList from '../assets/data/quiz-list';
+import { ChangeEvent, useState } from 'react';
 
 const VisitorHome = () => {
-  const publicQuizzes = [...quizList.getAll(), ...quizList.getAll()];
+  const publicQuizzes = [...quizList.getAll()];
 
+  const [inputValue, setInputValue] = useState('');
+  const [quizzes, setQuizzes] = useState(publicQuizzes);
+
+  const handleChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSearchClick = () => {
+    setQuizzes(
+      publicQuizzes.filter((item) =>
+        inputValue.trim() ? item.title.toLowerCase().includes(inputValue.toLowerCase()) : true,
+      ),
+    );
+    console.log(quizzes, 'quizzes');
+  };
+  console.log(inputValue, 'inputValue');
   return (
     <Helmet title="Home">
       <Container className="home">
         <div className="home-search">
           <div className="home-search__input">
             <SearchIcon className="home-search__input__icon" />
-            <input type="text" />
-            <Button className="home-search__input__button">Search</Button>
+            <input type="text" onChange={handleChangeInput} value={inputValue} />
+            <Button className="home-search__input__button" onClick={handleSearchClick}>
+              Search
+            </Button>
           </div>
         </div>
         <Wrapper className="home-result-wrapper">
-          <QuizList quizzes={publicQuizzes} />
+          <QuizList quizzes={quizzes} />
         </Wrapper>
       </Container>
     </Helmet>
--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -21,7 +21,7 @@ const Router = () => {
 
   return (
     <BrowserRouter>
-      {!isAuth ? (
+      {isAuth ? (
         <>
           <UserHeader />
           <UserRouting />
